68dc5097bf677634868acfa58747f23f9b4f2b37
Timothy-Hoogeveen <9020467@student.zadkine.nl>
2023-03-14 13:14:02 +0100

    opdracht_2.1

--- Opdracht_1.1/app.js
+++ Opdracht_1.1/app.js
@@ -1,3 +1,3 @@
-let title = document.getElementById("Java")
+let title = document.getElementById("Java") 
 title.style.color = "red"
 title.style.fontWeight = "bold"
---
+++ Opdracht_2.1/app.js
@@ -0,0 +1,11 @@
+function name() {
+    let title = document.getElementById("kleur_aanpassen")
+    title.style.color = "red";
+}
+ name()
+
+
+
+
+
+
