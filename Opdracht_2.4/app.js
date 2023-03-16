3b7c6b8fef0717117885c3408e09bd0b33c392fe
Timothy-Hoogeveen <9020467@student.zadkine.nl>
2023-03-16 10:04:30 +0100

    Opdracht_2.4

---
+++ Opdracht_2.4/app.js
@@ -0,0 +1,24 @@
+function calculate(num1, num2) {
+    let answer = num1 + num2;
+    console.log(answer);
+  }
+  
+  calculate(26,76);
+
+
+
+
+function multiplyby5(num1) {
+  let answer = num1 * 5;
+  console.log(answer);
+}
+
+multiplyby5(34);
+
+
+function calculateMinutes(num1 ) {
+    let answer = num1 / 60;
+    console.log(answer);
+  }
+  
+  calculateMinutes(3600);
