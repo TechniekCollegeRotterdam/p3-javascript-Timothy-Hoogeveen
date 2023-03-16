2fc7663c42161180e947bac3b0a24a982ecb116a
Timothy-Hoogeveen <9020467@student.zadkine.nl>
2023-03-16 10:33:35 +0100

    Opdracht_3.1

---
+++ Opdracht_3.1/app.js
@@ -0,0 +1,10 @@
+let h1 = document.getElementById("text");
+let budget = 100;
+let product = 60;
+
+if (budget > product) {
+h1.innerText = "U heeft genoeg geld!"
+
+} else {
+    h1.innerText = "U heeft te weinig geld!"
+}
