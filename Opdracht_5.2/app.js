4bf2743f8e5dabd4a0a8506abed6c75a12bcd123
Timothy-Hoogeveen <9020467@student.zadkine.nl>
2023-04-19 11:06:52 +0200

    Opdracht_5.2

--- Opdracht_5.2/app.js
+++ Opdracht_5.2/app.js
@@ -3,6 +3,7 @@ let lastScrollY = window.scrollY;
 
 window.addEventListener('scroll', () => {
 if (lastScrollY < window.scrollY) {
+    nav.classList.add('hide')
 } else {
  nav.classList.remove('hide')
 }
